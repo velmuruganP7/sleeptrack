35e6b3f916f116c857ae60ecdc249d7aeb355821
unknown <velmuruganp166@gmail.com>
2023-04-18 23:14:45 -0700

    completed

---
+++ sleep tracking app/studio/app/src/main/java/com/example/sleeptrackingapp/TimeLog.kt
@@ -0,0 +1,13 @@
+package com.example.sleeptrackingapp
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Date
+
+@Entity(tableName = "TimeLog")
+data class TimeLog(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val startTime: Date,
+    val stopTime: Date
+)
